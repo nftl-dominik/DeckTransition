3f3d928356ae7a4d0f4cb9c450620a6ece474b8c
Dominik Majda <dominik.majda@nftlearning.com>
2019-02-01 15:17:57 +0100

    Made constants var

--- Source/DeckConstants.swift
+++ Source/DeckConstants.swift
@@ -11,14 +11,14 @@ import UIKit
 public struct DeckConstants {
     
     /// The initial alpha value of the presented view controller's view
-    static let alphaValueForDimView: CGFloat = 0.4
+    static var alphaValueForDimView: CGFloat = 0.4
     
     /// On iPhone X/X Plus this will have additional space for safe area
-    public static let topInsetForPresentedView: CGFloat = 8
+    public static var topInsetForPresentedView: CGFloat = 8
 
     /// Describes how many pt user has to scroll in order to dismiss
-    public static let translationRequiredToDismiss: CGFloat = 200
+    public static var translationRequiredToDismiss: CGFloat = 200
     
     /// After this distance dismissing of modal window is slowing down, if not set dismissing is linear
-    public static let translationElasticThreshold: CGFloat? = nil
+    public static var translationElasticThreshold: CGFloat? = nil
 }
